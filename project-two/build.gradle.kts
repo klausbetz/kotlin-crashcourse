cfa7e4a928b2cd5ea81f2fd359acf3308a9f4f17
Klaus Betz <klaus.betz@deckweiss.at>
2021-10-31 11:37:28 +0100

    project-two - finished setup

---
+++ project-two/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+  kotlin("jvm") version "1.5.31"
+}
+
+group = "at.project"
+version = "1.0.0"
+
+repositories {
+  mavenCentral()
+}
+
+dependencies {
+  implementation(kotlin("stdlib"))
+}
---
+++ project-two/src/main/kotlin/at/project/two/Challenge.kt
@@ -0,0 +1,7 @@
+import java.io.File
+
+fun main() {
+  val content: String = File("input/example.txt").readText()
+
+  // your code goes here
+}
